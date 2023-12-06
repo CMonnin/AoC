91b1917f73cddd83326b43e7a4ff7b4046e2929c
Cian Monnin <cianmonnin@gmail.com>
2023-12-06 13:04:22 -0500

    day3 part1 works on test but not in actual input =(

--- 2023/py/day3/solution.py
+++ 2023/py/day3/solution.py
@@ -1,27 +1,79 @@
-def neighbour_checker(gear_map: dict):
-    valid_list = []
+def valid_populator(gear_map: dict):
+    valid_dict = {}
     for k, v in gear_map.items():
         if not v.isdigit():
-            co_ord_list = []
-            x_co_ord = k[0]
-            y_co_ord = k[1]
-
-            valid_list.append()
             print(k, v)
+            co_ord_list = []
+            x_y_co_ord = [k[0], k[1]]
+            surrounding_area = [
+                [-1, -1],
+                [-1, 0],
+                [0, -1],
+                [0, 0],
+                [1, 0],
+                [0, 1],
+                [1, 1],
+                [-1, 1],
+                [1, -1],
+            ]
+            for co_ord in surrounding_area:
+                co_ord_list.append([a + b for a, b in zip(x_y_co_ord, co_ord)])
+            for valid_co_cord in co_ord_list:
+                if not valid_dict.get(tuple(valid_co_cord)):
+                    valid_dict.update({tuple(valid_co_cord): "valid"})
+    return valid_dict
 
 
+# i want to keep adding to a list of numbers until it's done then i want to conncat
+# them together and check if any of there locations is valid. if yes add to a sum
 def part1():
-    with open("test1.txt") as f:
+    with open("input.txt") as f:
         lines = f.readlines()
         gear_map = {}
+        gear_sum = 0
         for y, line in enumerate(lines):
             line = line.rstrip()
             for x, c in enumerate(line):
                 if c != ".":
                     gear_map.update({(x, y): c})
-                    print(f"co-ord {x,y}")
-                    print(f"c{c}")
-        neighbour_checker(gear_map)
+        #           print(f"co-ord {x,y}")
+        #            print(f"c{c}")
+        # print(f" GEAR MAP  {gear_map}")
+        valid_dict = valid_populator(gear_map)
+
+        # print(valid_dict)
+        for y, line in enumerate(lines):
+            number_list = ["0"]
+            running_co_ord_list = []
+            line = line.rstrip()
+            for x, c in enumerate(line):
+                if c.isdigit():
+                    number_list.append(c)
+                    running_co_ord_list.append((x, y))
+                if not c.isdigit():
+                    actual_number = "0"
+                    for e in number_list:
+                        actual_number += e
+                    actual_number = int(actual_number)
+                    if any(x_y in valid_dict for x_y in running_co_ord_list):
+                        gear_sum += actual_number
+                        print(actual_number)
+                    number_list = ["0"]
+                    running_co_ord_list.clear()
+
+    print(gear_sum)
+
+
+def parse_string(s):
+    lines = s.split("\n")
+    numbers = []
+    coords = []
+    for i, line in enumerate(lines):
+        for j, char in enumerate(line):
+            if char.isdigit():
+                numbers.append(int(char))
+                coords.append((i, j))
+    return numbers, coords
 
 
 if __name__ == "__main__":
