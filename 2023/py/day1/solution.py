cbcc94946bc38a7bfaa1a668f530c07be021ad37
Cian Monnin <cianmonnin@gmail.com>
2024-11-24 08:40:39 -0500

    added 2024

--- 2023/py/day1/solution.py
+++ 2023/py/day1/solution.py
@@ -71,5 +71,4 @@ def part2():
 
 
 if __name__ == "__main__":
-    part1()
     part2()
---
+++ 2024/day09/generic.py
@@ -0,0 +1,14 @@
+def part1():
+    with open("input.txt") as f:
+        lines = f.readlines()
+        for line in lines:
+            
+    return
+def part2(): 
+    return 
+
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
