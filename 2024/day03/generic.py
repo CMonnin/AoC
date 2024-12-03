e9b474f7422fe5d2e7d9401e408d9332954fafe4
Cian <cianmonnin@gmail.com>
2024-12-03 09:02:50 -0500

    feature: day03 part1 solved

--- 2024/day03/generic.py
+++ 2024/day03/generic.py
@@ -1,12 +1,26 @@
+import re
+
+
 def part1():
     with open("input.txt") as f:
         lines = f.readlines()
+        sum = 0
         for line in lines:
-            
-    return
-def part2(): 
-    return 
+            regex_search = re.findall(r"mul\((\d{1,3}\,\d{1,3})\)", line)
+            converted = [[], []]
+            for pairs in regex_search:
+                x, y = pairs.split(",")
+                converted[0].append(x)
+                converted[1].append(y)
+            mul = map(lambda x, y: int(x) * int(y), converted[0], converted[1])
+            for i in mul:
+                sum += i
 
+        print(sum)
+
+
+def part2():
+    return
 
 
 if __name__ == "__main__":
