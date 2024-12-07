1b1ed83de29f1a08848c6ec65bf18c95d00e08d2
Cian <cianmonnin@gmail.com>
2024-12-07 08:18:45 -0500

    feature: day05 part1 solved

--- 2024/day05/generic.py
+++ 2024/day05/generic.py
@@ -1,12 +1,43 @@
+from collections import defaultdict
+
+
 def part1():
     with open("input.txt") as f:
-        lines = f.readlines()
-        for line in lines:
-            
-    return
-def part2(): 
-    return 
+        inst = f.readlines()[:1176]
+        f.seek(0)
+        lines = f.readlines()[1177:]
+        a_dict = defaultdict(list)
+        result = []
 
+        for rule in inst:
+            x, y = rule.split("|")
+            a_dict[int(y)].append(int(x.strip()))
+
+        for line in lines:
+            line = line.split(",")
+            line = list(map(lambda x: int(x.strip()), line))
+            is_valid = True
+
+            for count, element in enumerate(line):
+                for prior_element in a_dict[element]:
+                    if prior_element in line[count + 1 :]:
+                        is_valid = False
+                        break
+                if not is_valid:
+                    break
+
+            if is_valid:
+                x = len(line) // 2
+                result.append(line[x])
+
+        print("*******")
+        print(sum(result))
+
+
+def top_sort(arr, a_dict)
+
+def part2():
+    return
 
 
 if __name__ == "__main__":
