487eeb7c92df0784b4e7afdb31f64166778c1d3b
Cian Monnin <cianmonnin@gmail.com>
2025-01-21 13:29:53 -0500

    fixed typo, solved day7

--- 2015/day07/generic.py
+++ 2015/day07/generic.py
@@ -5,8 +5,8 @@ bitwise_operators = {
     "AND": operator.and_,  
     "LSHIFT": operator.lshift, 
     "RSHIFT": operator.rshift,
-    "NOT": operator.not_,
-    "OR": operator.invert,
+    "NOT": operator.invert,
+    "OR": operator.or_,
     "ID": lambda x: x
 }
 wires = {}
@@ -50,12 +50,42 @@ def part2():
     result = {}
     with open("input.txt") as f:
         lines = f.readlines()
-        for line in lines:
-            pass
-            
-    print(f"part 2: {result}") 
+        lines = f.readlines()
+        for line in lines: 
+            line = line.strip().split()
+            match line:
+                case x, "->", y:
+                    wires[y] = (bitwise_operators['ID'], x) 
+                case *x, op, z, "->", y:
+                    wires[y] = (bitwise_operators[op], *x, z)
+
+    @functools.cache
+    def solve_wire(w):
+        if isinstance(w, int) or w[0].isdigit():
+            return int(w) & 0xFFFF
+        
+        wire_data = wires[w]
+        operation = wire_data[0]
+        args = wire_data[1:]
+        
+        solved_args = []
+        for arg in args:
+            solved_value = solve_wire(arg)
+            solved_args.append(solved_value)
+        
+        result = operation(*solved_args)
+
+        if w == 'b':
+            final_result = 3176 
+        else:
+            final_result = result & 0xFFFF
+        
+        return final_result
+
+    return solve_wire("a")
 
 
+print(part2())
 
 if __name__ == "__main__":
     part1()
