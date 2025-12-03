be4960a53ac31d8220312db9ff03cc19253675ce
Cian <cianmonnin@gmail.com>
2025-12-03 08:38:22 -0500

    2025 02 p2 done

--- 2025/day02/solution.c
+++ 2025/day02/solution.c
@@ -26,16 +26,11 @@ int compare(char* str) {
     int len = strlen(str);
     int* factors = malloc(len * sizeof(int)); 
     int count = 0;
-    
+    // get the factors
     for (long i = 1; i < len; i++) {
         if (len % i == 0) {
             factors[count++] = i;
         }
-    }
-    
-    if (count == 0) {  
-        free(factors);
-        return 0;
     }
     
     factors = realloc(factors, count * sizeof(int));
@@ -49,6 +44,7 @@ int compare(char* str) {
         
         int repetitions = len / pattern_len;
         char* reconstructed = malloc(len + 1);
+        // init empty string
         reconstructed[0] = '\0';
         
         for (int j = 0; j < repetitions; j++) {
@@ -75,16 +71,17 @@ int compare(char* str) {
 long process_file(const char *filename) {
   FILE *file = fopen(filename, "r");
   if (!file) return 1;
-
   
   printf("\n=== Processing %s ===\n", filename);
   char *line = NULL;
   size_t len = 0;
   char array[1024][256];
   int size = 0;
-  long total=0;
-  int count= 0;
-
+  long total_halves = 0;
+  long total_patterns = 0;
+  int count_halves = 0;
+  int count_patterns = 0;
+  
   if (getline(&line, &len, file) != -1) {
     char *str_pt1, *str_pt2;
     char *token = strtok_r(line, ",", &str_pt1);
@@ -101,35 +98,43 @@ long process_file(const char *filename) {
       token = strtok_r(NULL,",",&str_pt1);
     }
   }
+  
   char *str1, *str2;
   for (int i=0;i<size;i++) {
     if (i%2==0) {
       str1=array[i];
-    // printf("%s\n","str1");
-    // printf("%s\n",str1);
-    }else {
+    } else {
       str2=array[i];
-    // printf("%s\n","str2");
-    // printf("%s\n",str2);
       long range_end = atol(str2)+1;
       for (long j=atol(str1); j<range_end; j++) {
         char current_num[256];
         sprintf(current_num,"%ld",j);
+        
         if (strlen(current_num)%2==0) {
-          if (check_halves(current_num)==0 ){
-            printf("%s\n",current_num);
-            total+=atol(current_num);
-            printf("%s%ld\n","current total: ",total);
-            count++;
+          if (check_halves(current_num)==0) {
+            //printf("%s (halves match)\n", current_num);
+            total_halves += atol(current_num);
+            //printf("Halves total: %ld\n", total_halves);
+            count_halves++;
           }
+        }
+        
+        if (compare(current_num)) {
+          //printf("%s (repeating pattern)\n", current_num);
+          total_patterns += atol(current_num);
+          //printf("Patterns total: %ld\n", total_patterns);
+          count_patterns++;
         }
       }
     }
   }
+  
   free(line);
   fclose(file);
-  printf("%s%d\n","count: ",count);
-  return total;
+  printf("\n=== Results ===\n");
+  printf("Halves count: %d, total: %ld\n", count_halves, total_halves);
+  printf("Patterns count: %d, total: %ld\n", count_patterns, total_patterns);
+  return 0;  
 }
 
 
